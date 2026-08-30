21e000384454d74b935dac94a35e277fa5c3750a
Sujan2515 <jeevansujan12@gmail.com>
2026-08-30 21:50:46 +0530

    Fix model path for AWS deployment

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -1,62 +1,100 @@
+import os
 import sys
 import pandas as pd
+
 from src.exception import CustomException
 from src.utils import load_object
 
 
 class PredictPipeline:
+
     def __init__(self):
         pass
 
-    def predict(self,features):
+    def predict(self, features):
+
         try:
-            model_path='artifacts\model.pkl'
-            preprocessor_path='artifacts\preprocessor.pkl'
-            model=load_object(file_path=model_path)
-            preprocessor=load_object(file_path=preprocessor_path)
-            data_scaled=preprocessor.transform(features)
-            preds=model.predict(data_scaled)
+            # Get the project root directory
+            project_root = os.path.dirname(
+                os.path.dirname(
+                    os.path.dirname(
+                        os.path.abspath(__file__)
+                    )
+                )
+            )
+
+            # Create OS-independent paths
+            model_path = os.path.join(
+                project_root,
+                "artifacts",
+                "model.pkl"
+            )
+
+            preprocessor_path = os.path.join(
+                project_root,
+                "artifacts",
+                "preprocessor.pkl"
+            )
+
+            print("MODEL PATH:", model_path)
+            print("PREPROCESSOR PATH:", preprocessor_path)
+
+            # Load model and preprocessor
+            model = load_object(file_path=model_path)
+            preprocessor = load_object(file_path=preprocessor_path)
+
+            # Transform input data
+            data_scaled = preprocessor.transform(features)
+
+            # Make prediction
+            preds = model.predict(data_scaled)
+
             return preds
 
         except Exception as e:
-            raise CustomException(e,sys)
+            raise CustomException(e, sys)
 
 
 class CustomData:
-    def __init__(self,
+
+    def __init__(
+        self,
         gender: str,
-        race_ethnicity:int,
-        parental_level_of_education,
+        race_ethnicity: str,
+        parental_level_of_education: str,
         lunch: str,
         test_preparation_course: str,
-        reading_score:int,
-        writing_score:int):
+        reading_score: int,
+        writing_score: int
+    ):
+
         self.gender = gender
-        self.race_ethnicity=race_ethnicity
-        self.test_preparation_course=test_preparation_course
-        self.parental_level_of_education=parental_level_of_education
-        self.lunch=lunch
-        self.reading_score=reading_score
-        self.writing_score=writing_score
+        self.race_ethnicity = race_ethnicity
+        self.test_preparation_course = test_preparation_course
+        self.parental_level_of_education = parental_level_of_education
+        self.lunch = lunch
+        self.reading_score = reading_score
+        self.writing_score = writing_score
 
     def get_data_as_data_frame(self):
+
         try:
-            custom_data_input_dict={
-                "gender":[self.gender],
-                "race_ethnicity":[self.race_ethnicity],
-                "parental_level_of_education":[self.parental_level_of_education],
-                "lunch":[self.lunch],
-                "test_preparation_course":[self.test_preparation_course],
-                "reading_score":[self.reading_score],
-                "writing_score":[self.writing_score]
+
+            custom_data_input_dict = {
+                "gender": [self.gender],
+                "race_ethnicity": [self.race_ethnicity],
+                "parental_level_of_education": [
+                    self.parental_level_of_education
+                ],
+                "lunch": [self.lunch],
+                "test_preparation_course": [
+                    self.test_preparation_course
+                ],
+                "reading_score": [self.reading_score],
+                "writing_score": [self.writing_score]
             }
+
             return pd.DataFrame(custom_data_input_dict)
 
         except Exception as e:
-            raise CustomException(e,sys)
-
-
-
-
-
-
+            raise CustomException(e, sys)
